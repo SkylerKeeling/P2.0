94de2a276fbb6b5b9f680c7e37c74145f753a157
skyler <skyler@skylers-MacBook-Pro.local>
2023-10-09 09:53:13 -0300

    layout changed completly, content gutted

--- P2.0/src/App.jsx
+++ P2.0/src/App.jsx
@@ -1,39 +1,16 @@
-import "./App.css"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
-import Home from "./components/Home"
-import Mywork from "./components/Mywork"
-import Contact from "./components/Contact"
+// Bringing in the required import from 'react-router-dom'
+import {Outlet} from "react-router-dom"
+import Navbar from "./pages/Navbar"
 
-const routes = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/header",
-    element: <Header />,
-  },
-  {
-    path: "/footer",
-    element: <Footer />,
-  },
-  {
-    path: "/navbar",
-    element: <Navbar />,
-  },
-  {
-    path: "/mywork",
-    element: <Mywork />,
-  },
-])
-
-const App = () => {
+function App() {
+  // The Outlet component will conditionally swap between the different pages according to the URL
   return (
-    <main>
-      <RouterProvider router={routes} fallbackElement={<Home />} />
-    </main>
+    <>
+      <Navbar />
+      <main className="mx-3">
+        <Outlet />
+      </main>
+    </>
   )
 }
 
