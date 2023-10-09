559348317346d14ed8aa6d557032c135935380d8
skyler <skyler@skylers-MacBook-Pro.local>
2023-10-09 14:21:10 -0300

    two cards added to portfolio

--- P2.0/src/pages/Portfolio.jsx
+++ P2.0/src/pages/Portfolio.jsx
@@ -1,17 +1,21 @@
 import cyberScript from "../assets/cyberpic.png"
+import pokePlaylistPic from "../assets/PokePlaylist-Search.png"
 
 export default function Blog() {
   return (
     <div>
       <div class="flex flex-col items-stretch">
         {/* Card */}
-        <div class="max-w-sm m-4 bg-white border self-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div
+          style={{maxWidth: "450px"}}
+          class="max-w-sm m-4 bg-white border self-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+        >
           <a href="#">
             <img
               class="rounded-t-lg m-5 "
               src={cyberScript}
               alt=""
-              style={{maxWidth: "250px"}}
+              style={{maxWidth: "400px"}}
             />
           </a>
           <div class="p-5">
@@ -23,7 +27,7 @@ export default function Blog() {
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
             <a
               href="https://github.com/YggdrasilJL/code-name-project-3"
-              class="inline-flex no-underline items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              class="inline-flex no-underline items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               View repository
               <svg
@@ -45,7 +49,111 @@ export default function Blog() {
           </div>
         </div>
         {/* Card */}
-        <div class="max-w-sm m-4 bg-white border self-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div
+          style={{maxWidth: "450px"}}
+          class="max-w-sm m-4 bg-white border self-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+        >
+          <a href="#">
+            <img class="rounded-t-lg" src={pokePlaylistPic} alt="" />
+          </a>
+          <div class="p-5">
+            <a href="#">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                PokePlaylist
+              </h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
+            <a
+              href="https://stms15.github.io/PokePlaylist-app/"
+              class="inline-flex m-1 items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              View application
+              <svg
+                class="w-3.5 h-3.5 ml-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+            <a
+              href="https://github.com/stms15/PokePlaylist-app"
+              class="inline-flex m-1 no-underline items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              View repository
+              <svg
+                class="w-3.5 h-3.5 ml-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+        {/* Card */}
+        <div
+          style={({maxWidth: "450px"}, {minWidth: "450px"})}
+          class="max-w-sm m-4 bg-white border self-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+        >
+          <a href="#">
+            <img
+              class="rounded-t-lg"
+              src="/docs/images/blog/image-1.jpg"
+              alt=""
+            />
+          </a>
+          <div class="p-5">
+            <a href="#">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Social-Network-API
+              </h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
+            <a
+              href="https://github.com/SkylerKeeling/Social-Network-API"
+              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              View repository
+              <svg
+                class="w-3.5 h-3.5 ml-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+        {/* Card */}
+        <div
+          style={{maxWidth: "450px"}}
+          class="max-w-sm m-4 bg-white border self-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+        >
           <a href="#">
             <img
               class="rounded-t-lg"
@@ -87,7 +195,10 @@ export default function Blog() {
           </div>
         </div>
         {/* Card */}
-        <div class="max-w-sm m-4 bg-white border self-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div
+          style={{maxWidth: "450px"}}
+          class="max-w-sm m-4 bg-white border self-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+        >
           <a href="#">
             <img
               class="rounded-t-lg"
@@ -129,91 +240,10 @@ export default function Blog() {
           </div>
         </div>
         {/* Card */}
-        <div class="max-w-sm m-4 bg-white border self-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <a href="#">
-            <img
-              class="rounded-t-lg"
-              src="/docs/images/blog/image-1.jpg"
-              alt=""
-            />
-          </a>
-          <div class="p-5">
-            <a href="#">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <a
-              href="#"
-              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Read more
-              <svg
-                class="w-3.5 h-3.5 ml-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
-          </div>
-        </div>
-        {/* Card */}
-        <div class="max-w-sm m-4 bg-white border self-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <a href="#">
-            <img
-              class="rounded-t-lg"
-              src="/docs/images/blog/image-1.jpg"
-              alt=""
-            />
-          </a>
-          <div class="p-5">
-            <a href="#">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <a
-              href="#"
-              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Read more
-              <svg
-                class="w-3.5 h-3.5 ml-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
-          </div>
-        </div>
-        {/* Card */}
-        <div class="max-w-sm m-4 bg-white border self-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div
+          style={{maxWidth: "450px"}}
+          class="max-w-sm m-4 bg-white border self-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+        >
           <a href="#">
             <img
               class="rounded-t-lg"
