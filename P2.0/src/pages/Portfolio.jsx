04877b013fe112fe54c4bc128d211bfed6488c8d
skyler <skyler@skylers-MacBook-Pro.local>
2023-10-09 20:43:09 -0300

    contact page functions properly, email is recieved

--- P2.0/src/pages/Portfolio.jsx
+++ P2.0/src/pages/Portfolio.jsx
@@ -151,7 +151,7 @@ export default function Blog() {
         </div>
         {/* Card */}
         <div
-          style={{maxWidth: "450px"}}
+          style={({maxWidth: "450px"}, {minWidth: "450px"})}
           class="max-w-sm m-4 bg-white border self-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
         >
           <a href="#">
@@ -164,18 +164,15 @@ export default function Blog() {
           <div class="p-5">
             <a href="#">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
+                MySQL-Employee-Tracker
               </h5>
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
             <a
-              href="#"
-              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              href="https://github.com/SkylerKeeling/mySQL-employee-tracker"
+              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Read more
+              Visit repository
               <svg
                 class="w-3.5 h-3.5 ml-2"
                 aria-hidden="true"
@@ -196,7 +193,7 @@ export default function Blog() {
         </div>
         {/* Card */}
         <div
-          style={{maxWidth: "450px"}}
+          style={({maxWidth: "450px"}, {minWidth: "450px"})}
           class="max-w-sm m-4 bg-white border self-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
         >
           <a href="#">
@@ -209,18 +206,15 @@ export default function Blog() {
           <div class="p-5">
             <a href="#">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
+                ReadMe Generator
               </h5>
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
             <a
-              href="#"
-              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              href="https://github.com/SkylerKeeling/ReadMe-generator"
+              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Read more
+              View repository
               <svg
                 class="w-3.5 h-3.5 ml-2"
                 aria-hidden="true"
@@ -241,7 +235,7 @@ export default function Blog() {
         </div>
         {/* Card */}
         <div
-          style={{maxWidth: "450px"}}
+          style={({maxWidth: "450px"}, {minWidth: "450px"})}
           class="max-w-sm m-4 bg-white border self-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
         >
           <a href="#">
@@ -254,18 +248,15 @@ export default function Blog() {
           <div class="p-5">
             <a href="#">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
+                E-Com-Back-End
               </h5>
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
             <a
-              href="#"
-              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              href="https://github.com/SkylerKeeling/E-Com-Back-End"
+              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Read more
+              View repository
               <svg
                 class="w-3.5 h-3.5 ml-2"
                 aria-hidden="true"
