e795c4736fc3acf02613cdd50e9864494876714e
skyler <skyler@skylers-MacBook-Pro.local>
2023-10-10 20:39:57 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
+  base: "/P2.0/",
   plugins: [react()],
   server: {
     port: process.env.PORT || 8080,
