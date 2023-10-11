076b34e19c3e6738af06c16c71df7abbab6ac032
skyler <skyler@skylers-MacBook-Pro.local>
2023-10-11 18:09:06 -0300

    bug fixed

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: "/P2.0/",
   plugins: [react()],
   server: {
     port: process.env.PORT || 8080,
